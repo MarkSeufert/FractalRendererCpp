5316bcccc5b8b4775120651d90d3e0c061fb005e
Mark Seufert <markseufert1@gmail.com>
2021-01-07 10:40:44 -0500

    Added fractal interface and mandelbrot implementation

--- Fractals/include/Mandelbrot.h
+++ Fractals/include/Mandelbrot.h
@@ -1,6 +1,33 @@
+#pragma once
+
+#include "Renderer/FractalInterface.h"
+
 /*
 This file contains the mathematical implementation of the burning ship fractal.
 For more details of how this algorithm works, reading this is helpful: https://en.wikipedia.org/wiki/Burning_Ship_fractal
 */
 
-// TODO: actually implement this file
+class Mandelbrot : public FractalInterface
+{
+public:
+	double Evaluate(double real, double imaginary)
+	{
+		// At a high level, compute (zn+1 = zn ^ 2 + c) until zn+1 has a magnitude > 2 or until max iterations
+		double zn_real = 0, zn_imag = 0;
+		double znplus1_real = 0, znplus1_imag = 0;
+		for (double i = 0; i < maxIterations_; i++) {
+			// Calculate zn+1 as zn ^ 2 + c
+			znplus1_real = (zn_real * zn_real) - (zn_imag * zn_imag) + real;
+			znplus1_imag = 2 * zn_real * zn_imag + imaginary;
+
+			// Check if the magnitude of zn+1 is greater than 2
+			// Which is equal to the square magnitude is greater than 4 (more efficient)
+			if ((znplus1_real * znplus1_real) + (znplus1_imag * znplus1_imag) > 4) {
+				return i / maxIterations_;
+			}
+		}
+
+		// If after maxIterations, the point did not return, it is in the set
+		return 1;
+	}
+};
--- Renderer/include/Renderer/FractalInterface.h
+++ Renderer/include/Renderer/FractalInterface.h
@@ -1,3 +1,25 @@
 /*
 This class creates an interface that a fractal implementation must follow.
 */
+
+class FractalInterface
+{
+public:
+	/*
+	Calculates this fractal's algorithm given a (real,imaginary) point. Must return a value 
+	between 0 and 1, where a value closer to 1 means closer to being inside the fractal. A value
+	equal to 1 means the point is inside the set.
+	*/
+	virtual double Evaluate(double real, double imaginary) = 0;
+
+	/*
+	Sets the max number of iterations that the Evaluate() method can complete before returning.
+	*/
+	void SetMaxIterations(int maxIterations)
+	{
+		maxIterations_ = maxIterations;
+	}
+
+private:
+	int maxIterations_ = 100;
+};
